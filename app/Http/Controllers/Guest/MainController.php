7d6ad72845230985fa121413f889256a8a65af86
hoaiphuong2527 <hoaiphuong2527@gmail.com>
2018-07-26 12:02:04 +0700

    update master view

--- app/Http/Controllers/Guest/MainController.php
+++ app/Http/Controllers/Guest/MainController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Guest;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MainController extends Controller
 {
     public function main()
     {
-        return view('layout.main');
+        return view('guest.home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main','MainController@main');
+Route::get('/','Guest\MainController@main');
