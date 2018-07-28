bbf23b479b718dd78107c233a568d219ce65597a
hoaiphuong2527 <hoaiphuong2527@gmail.com>
2018-07-28 22:36:13 +0700

    update_view

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -68,6 +68,11 @@
 <!-- Banner -->
 <section class="banner bgwhite p-t-40 p-b-40">
     <div class="container">
+        <div class="sec-title p-b-60">
+            <h3 class="m-text5 t-center">
+                Featured Products
+            </h3>
+        </div>
         <div class="row">
             <div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
                 <!-- block1 -->
@@ -77,7 +82,7 @@
                     <div class="block1-wrapbtn w-size2">
                         <!-- Button -->
                         <a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-                            Dresses
+                           More
                         </a>
                     </div>
                 </div>
@@ -89,7 +94,7 @@
                     <div class="block1-wrapbtn w-size2">
                         <!-- Button -->
                         <a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-                            Sunglasses
+                                More
                         </a>
                     </div>
                 </div>
@@ -103,7 +108,7 @@
                     <div class="block1-wrapbtn w-size2">
                         <!-- Button -->
                         <a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-                            Watches
+                                More
                         </a>
                     </div>
                 </div>
@@ -115,7 +120,7 @@
                     <div class="block1-wrapbtn w-size2">
                         <!-- Button -->
                         <a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-                            Footerwear
+                                More
                         </a>
                     </div>
                 </div>
@@ -129,7 +134,7 @@
                     <div class="block1-wrapbtn w-size2">
                         <!-- Button -->
                         <a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-                            Bags
+                                More
                         </a>
                     </div>
                 </div>
@@ -165,7 +170,7 @@
     <div class="container">
         <div class="sec-title p-b-60">
             <h3 class="m-text5 t-center">
-                Featured Products
+                Latest Products
             </h3>
         </div>
 
@@ -624,6 +629,7 @@
                 </div>
             </div>
         </div>
+        <a href="" style="float: right;">Read more</a>
     </div>
 </section>
 
@@ -750,36 +756,5 @@
 
 <!-- Shipping -->
 <section class="shipping bgwhite p-t-62 p-b-46">
-    <div class="flex-w p-l-15 p-r-15">
-        <div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
-            <h4 class="m-text12 t-center">
-                Free Delivery Worldwide
-            </h4>
-
-            <a href="#" class="s-text11 t-center">
-                Click here for more info
-            </a>
-        </div>
-
-        <div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 bo2 respon2">
-            <h4 class="m-text12 t-center">
-                30 Days Return
-            </h4>
-
-            <span class="s-text11 t-center">
-                Simply return it within 30 days for an exchange.
-            </span>
-        </div>
-
-        <div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
-            <h4 class="m-text12 t-center">
-                Store Opening
-            </h4>
-
-            <span class="s-text11 t-center">
-                Shop open from Monday to Sunday
-            </span>
-        </div>
-    </div>
 </section>
 @endsection
--- resources/views/guest/product-detail.blade.php
+++ resources/views/guest/product-detail.blade.php
@@ -51,28 +51,12 @@
 						</div>
 
 						<div class="rs2-select2 rs3-select2 bo4 of-hidden w-size16">
-							<select class="selection-2" name="size">
+							<select class="selection-2 form-control" name="size" style="border: none">
 								<option>Choose an option</option>
 								<option>Size S</option>
 								<option>Size M</option>
 								<option>Size L</option>
 								<option>Size XL</option>
-							</select>
-						</div>
-					</div>
-
-					<div class="flex-m flex-w">
-						<div class="s-text15 w-size15 t-center">
-							Color
-						</div>
-
-						<div class="rs2-select2 rs3-select2 bo4 of-hidden w-size16">
-							<select class="selection-2" name="color">
-								<option>Choose an option</option>
-								<option>Gray</option>
-								<option>Red</option>
-								<option>Black</option>
-								<option>Blue</option>
 							</select>
 						</div>
 					</div>
@@ -100,44 +84,10 @@
 						</div>
 					</div>
 				</div>
-
-				<div class="p-b-45">
-					<span class="s-text8 m-r-35">SKU: MUG-01</span>
-					<span class="s-text8">Categories: Mug, Design</span>
-				</div>
-
 				<!--  -->
 				<div class="wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content">
 					<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
 						Description
-						<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
-						<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
-					</h5>
-
-					<div class="dropdown-content dis-none p-t-15 p-b-23">
-						<p class="s-text8">
-							Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-						</p>
-					</div>
-				</div>
-
-				<div class="wrap-dropdown-content bo7 p-t-15 p-b-14">
-					<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
-						Additional information
-						<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
-						<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
-					</h5>
-
-					<div class="dropdown-content dis-none p-t-15 p-b-23">
-						<p class="s-text8">
-							Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-						</p>
-					</div>
-				</div>
-
-				<div class="wrap-dropdown-content bo7 p-t-15 p-b-14">
-					<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
-						Reviews (0)
 						<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
 						<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
 					</h5>
